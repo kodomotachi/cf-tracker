a7a0f21caea9306f4f47d84af9490a1671228f16
kodomotachi <kodomotachi2k4@gmail.com>
2023-07-25 20:39:47 +0700

    Upate to testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Date: 07.05.2023 at Thu Duc City, Ho Chi Minh City, Vietnam.
+        </p>
+        <p>
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          Author: KodomoTachi and developers: Sterryn, Shirokun and zuyyy1412
+        </p>
+        <p>
+          Hello world. This is my project about tracking performance on Codeforces.
         </p>
         <a
           className="App-link"
@@ -17,6 +24,26 @@ function App() {
         >
           Learn React
         </a>
+        <p>
+          Coming soon.
+        </p>
+        <table>
+          <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+          </tr>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+          </tr>
+          <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+          </tr>
+          </table>
       </header>
     </div>
   );
