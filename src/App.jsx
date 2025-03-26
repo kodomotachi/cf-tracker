c8148efe2628ce5b0355ecd74bd94bb1cbc9ce85
kodomotachi <kodomotachi2k4@gmail.com>
2025-03-26 20:51:24 +0700

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './pages/Home/Home'
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path="/dashboard" exact element={<Home />} />
+    </Routes>
+  </Router>
+)
 
 const App = () => {
   return (
     <div>
-      <Home />
+      {routes}
     </div>
   )
 }
