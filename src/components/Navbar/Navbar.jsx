f14d45da25ef1e5f59b2e60b0d3f06295e7041a4
kodomotachi <kodomotachi2k4@gmail.com>
2025-04-06 21:14:35 +0700

    add something

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-	<div>Navbar</div>
+    <div className="bg-black flex items-center justify-between px-14 py-2 drop-shadow">
+      <h2 className="text-xl font-medium text-white py-2">cftracker</h2>
+    </div>
   )
 }
 
